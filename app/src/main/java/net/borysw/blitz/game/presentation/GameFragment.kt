1c578fddd62fb8d0d37d16ea767c53e6afd71917
Borys Wojciechowski <borwoj@gmail.com>
2018-11-21 23:08:21 +0100

    refactor

--- app/src/main/java/net/borysw/blitz/game/presentation/GameFragment.kt
+++ app/src/main/java/net/borysw/blitz/game/presentation/GameFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.transition.ChangeBounds
-import androidx.transition.TransitionManager
+import androidx.transition.TransitionManager.beginDelayedTransition
 import com.google.android.material.snackbar.Snackbar
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_game.root
@@ -71,13 +71,12 @@ class GameFragment : Fragment() {
   }
 
   private fun showPlayerActive(gameStatus: GameStatus.Status) {
-    val constraintSet = ConstraintSet()
-    constraintSet.clone(context, getConstraintSet(gameStatus))
-    val transition = ChangeBounds()
-    transition.interpolator = OvershootInterpolator(1f)
-    transition.duration = 250
-    TransitionManager.beginDelayedTransition(root, transition)
-    constraintSet.applyTo(root)
+    val transition = ChangeBounds().apply {
+      interpolator = OvershootInterpolator(1f)
+      duration = 250
+    }
+    beginDelayedTransition(root, transition)
+    getConstraintSet(gameStatus).applyTo(root)
 
     if (gameStatus == PLAYER_A) {
       timerA.isActive = true
@@ -87,22 +86,24 @@ class GameFragment : Fragment() {
     timerB.isActive = !timerA.isActive
   }
 
-  private fun getConstraintSet(gameStatus: GameStatus.Status) = when (gameStatus) {
-    INITIAL -> fragment_game
-    FINISHED -> fragment_game_player_finish
-    PLAYER_A -> fragment_game_player_a
-    PLAYER_B -> fragment_game_player_b
+  private fun getConstraintSet(gameStatus: GameStatus.Status): ConstraintSet {
+    val layoutResId = when (gameStatus) {
+      INITIAL -> fragment_game
+      FINISHED -> fragment_game_player_finish
+      PLAYER_A -> fragment_game_player_a
+      PLAYER_B -> fragment_game_player_b
+    }
+    return ConstraintSet().apply { clone(context, layoutResId) }
   }
 
   private fun showGameFinished() {
     Snackbar.make(timerA, "Game finished", Snackbar.LENGTH_INDEFINITE).show()
-    val constraintSet = ConstraintSet()
-    constraintSet.clone(context, getConstraintSet(FINISHED))
-    val transition = ChangeBounds()
-    transition.interpolator = AnticipateOvershootInterpolator()
-    transition.duration = 1000
-    TransitionManager.beginDelayedTransition(root, transition)
-    constraintSet.applyTo(root)
+    val transition = ChangeBounds().apply {
+      interpolator = AnticipateOvershootInterpolator()
+      duration = 1000
+    }
+    beginDelayedTransition(root, transition)
+    getConstraintSet(FINISHED).applyTo(root)
 
     start.setImageResource(R.drawable.ic_replay_black_24dp)
   }
