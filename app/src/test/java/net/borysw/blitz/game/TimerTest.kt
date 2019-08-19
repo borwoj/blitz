3cd193482564381d7d2f75a5f5c776f357264e1e
Borys Wojciechowski <borwoj@gmail.com>
2019-08-19 23:25:07 +0200

    test

--- app/src/test/java/net/borysw/blitz/game/TimerTest.kt
+++ app/src/test/java/net/borysw/blitz/game/TimerTest.kt
@@ -1,6 +1,5 @@
 package net.borysw.blitz.game
 
-
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertThrows
@@ -10,125 +9,125 @@ import org.junit.jupiter.api.Test
 
 internal class TimerTest {
 
-  @Test
-  @DisplayName("should keep initial time")
-  fun initialTime() {
-    val testedObj = Timer(1)
+    @Test
+    @DisplayName("should keep initial time")
+    fun initialTime() {
+        val testedObj = Timer().apply { initialTime = 1 }
 
-    assertEquals(1, testedObj.initialTime)
-  }
+        assertEquals(1, testedObj.initialTime)
+    }
 
-  @Test
-  @DisplayName("when advancing time, should decrement remaining time")
-  fun advance() {
-    val testedObj = Timer(1)
-    testedObj.advance()
+    @Test
+    @DisplayName("when advancing time, should decrement remaining time")
+    fun advance() {
+        val testedObj = Timer().apply { initialTime = 1 }
+        testedObj.advance()
 
-    assertEquals(0, testedObj.remainingTime)
-  }
+        assertEquals(0, testedObj.remainingTime)
+    }
 
-  @Test
-  @DisplayName("when advancing time by value, should subtract this value from remaining time")
-  fun advanceBy() {
-    val testedObj = Timer(3)
-    testedObj.advanceBy(2)
+    @Test
+    @DisplayName("when advancing time by value, should subtract this value from remaining time")
+    fun advanceBy() {
+        val testedObj = Timer().apply { initialTime = 3 }
+        testedObj.advanceBy(2)
 
-    assertEquals(1, testedObj.remainingTime)
-  }
+        assertEquals(1, testedObj.remainingTime)
+    }
 
-  @Test
-  @DisplayName("when advancing time while time is already over, should throw exception")
-  fun advanceIllegal() {
-    val testedObj = Timer(0)
+    @Test
+    @DisplayName("when advancing time while time is already over, should throw exception")
+    fun advanceIllegal() {
+        val testedObj = Timer().apply { initialTime = 0 }
 
-    assertThrows(IllegalStateException::class.java) { testedObj.advance() }
-  }
+        assertThrows(IllegalStateException::class.java) { testedObj.advance() }
+    }
 
-  @Test
-  @DisplayName("when advancing time by value and this value is greater than remaining time, should throw exception")
-  fun advanceByIllegal() {
-    val testedObj = Timer(0)
+    @Test
+    @DisplayName("when advancing time by value and this value is greater than remaining time, should throw exception")
+    fun advanceByIllegal() {
+        val testedObj = Timer().apply { initialTime = 0 }
 
-    assertThrows(IllegalStateException::class.java) { testedObj.advanceBy(1) }
-  }
+        assertThrows(IllegalStateException::class.java) { testedObj.advanceBy(1) }
+    }
 
-  @Test
-  @DisplayName("when advancing time, should increment elapsed time")
-  fun elapsedTime() {
-    val testedObj = Timer(1)
-    testedObj.advance()
+    @Test
+    @DisplayName("when advancing time, should increment elapsed time")
+    fun elapsedTime() {
+        val testedObj = Timer().apply { initialTime = 1 }
+        testedObj.advance()
 
-    assertEquals(1, testedObj.elapsedTime)
-  }
+        assertEquals(1, testedObj.elapsedTime)
+    }
 
-  @Test
-  @DisplayName("when advancing time by value, should add this value to elapsed time")
-  fun elapsedTimeAdvanceBy() {
-    val testedObj = Timer(2)
-    testedObj.advanceBy(2)
+    @Test
+    @DisplayName("when advancing time by value, should add this value to elapsed time")
+    fun elapsedTimeAdvanceBy() {
+        val testedObj = Timer().apply { initialTime = 2 }
+        testedObj.advanceBy(2)
 
-    assertEquals(2, testedObj.elapsedTime)
-  }
+        assertEquals(2, testedObj.elapsedTime)
+    }
 
-  @Test
-  @DisplayName("when created, remaining time should be equal to initial time")
-  fun getRemainingTime() {
-    val testedObj = Timer(1)
+    @Test
+    @DisplayName("when created, remaining time should be equal to initial time")
+    fun getRemainingTime() {
+        val testedObj = Timer().apply { initialTime = 1 }
 
-    assertEquals(1, testedObj.remainingTime)
-  }
+        assertEquals(1, testedObj.remainingTime)
+    }
 
-  @Test
-  @DisplayName("when reset, remaining time should be equal to initial time and elapsed time should be 0")
-  fun reset() {
-    val testedObj = Timer(1)
-    testedObj.advance()
-    testedObj.reset()
+    @Test
+    @DisplayName("when reset, remaining time should be equal to initial time and elapsed time should be 0")
+    fun reset() {
+        val testedObj = Timer().apply { initialTime = 1 }
+        testedObj.advance()
+        testedObj.reset()
 
-    assertEquals(1, testedObj.remainingTime)
-    assertEquals(0, testedObj.elapsedTime)
-  }
+        assertEquals(1, testedObj.remainingTime)
+        assertEquals(0, testedObj.elapsedTime)
+    }
 
-  @Test
-  @DisplayName("should calculate progress as a relation of elapsed time to sum of elapsed time and remaining time")
-  fun progress() {
-    val testedObj = Timer(4)
+    @Test
+    @DisplayName("should calculate progress as a relation of elapsed time to sum of elapsed time and remaining time")
+    fun progress() {
+        val testedObj = Timer().apply { initialTime = 4 }
 
-    assertEquals(0f, testedObj.progress)
-    testedObj.advance()
-    assertEquals(0.25f, testedObj.progress)
-    testedObj.advance()
-    assertEquals(0.5f, testedObj.progress)
-    testedObj.addTime(1)
-    assertEquals(0.4f, testedObj.progress)
-    testedObj.advanceBy(3)
-    assertEquals(1f, testedObj.progress)
-  }
+        assertEquals(0f, testedObj.progress)
+        testedObj.advance()
+        assertEquals(0.25f, testedObj.progress)
+        testedObj.advance()
+        assertEquals(0.5f, testedObj.progress)
+        testedObj.addTime(1)
+        assertEquals(0.4f, testedObj.progress)
+        testedObj.advanceBy(3)
+        assertEquals(1f, testedObj.progress)
+    }
 
-  @Test
-  @DisplayName("when adding time, should increase remaining time but not initial time")
-  fun addTime() {
-    val testedObj = Timer(1)
-    testedObj.addTime(2)
+    @Test
+    @DisplayName("when adding time, should increase remaining time but not initial time")
+    fun addTime() {
+        val testedObj = Timer().apply { initialTime = 1 }
+        testedObj.addTime(2)
 
-    assertEquals(3, testedObj.remainingTime)
-    assertEquals(1, testedObj.initialTime)
-  }
+        assertEquals(3, testedObj.remainingTime)
+        assertEquals(1, testedObj.initialTime)
+    }
 
-  @Test
-  @DisplayName("when remaining time is 0, should return true")
-  fun isTimeOverYes() {
-    val testedObj = Timer(1)
-    testedObj.advance()
+    @Test
+    @DisplayName("when remaining time is 0, should return true")
+    fun isTimeOverYes() {
+        val testedObj = Timer().apply { initialTime = 1 }
+        testedObj.advance()
 
-    assertTrue(testedObj.isTimeOver())
-  }
+        assertTrue(testedObj.isTimeOver())
+    }
 
-  @Test
-  @DisplayName("when remaining time is not 0, should return false")
-  fun isTimeOverNo() {
-    val testedObj = Timer(1)
+    @Test
+    @DisplayName("when remaining time is not 0, should return false")
+    fun isTimeOverNo() {
+        val testedObj = Timer().apply { initialTime = 1 }
 
-    assertFalse(testedObj.isTimeOver())
-  }
+        assertFalse(testedObj.isTimeOver())
+    }
 }
