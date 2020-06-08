cb35289de3aa84b430a75bef5ae1e29e23e46681
Borys Wojciechowski <borwoj@gmail.com>
2020-06-08 20:46:53 +0200

    update

--- app/src/test/java/net/borysw/blitz/game/TimerImplTest.kt
+++ app/src/test/java/net/borysw/blitz/game/TimerImplTest.kt
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-// TODO display names
 internal class TimerImplTest {
     private lateinit var testedObj: TimerImpl
 
@@ -17,21 +17,24 @@ internal class TimerImplTest {
     }
 
     @Test
+    @DisplayName("when initial time set, initial time and remaining time should be equal")
     fun getInitialTime() {
-        testedObj.initialTime = 5
+        testedObj.initialTime = 1
 
-        assertEquals(5, testedObj.initialTime)
-        assertEquals(5, testedObj.remainingTime)
+        assertEquals(1, testedObj.initialTime)
+        assertEquals(1, testedObj.remainingTime)
     }
 
     @Test
+    @DisplayName("should set remaining time")
     fun getRemainingTime() {
-        testedObj.remainingTime = 5
+        testedObj.remainingTime = 1
 
-        assertEquals(5, testedObj.remainingTime)
+        assertEquals(1, testedObj.remainingTime)
     }
 
     @Test
+    @DisplayName("when remaining time is 0, time should be over")
     fun isTimeOver() {
         testedObj.remainingTime = 0
 
@@ -39,37 +42,41 @@ internal class TimerImplTest {
     }
 
     @Test
+    @DisplayName("when remaining time is not 0, time should not be over")
     fun isTimeOverNot() {
-        testedObj.remainingTime = 5
+        testedObj.remainingTime = 1
 
         assertFalse(testedObj.isTimeOver)
     }
 
     @Test
+    @DisplayName("when time advanced, should decrement remaining time")
     fun advanceTime() {
-        testedObj.initialTime = 5
+        testedObj.initialTime = 1
 
         testedObj.advanceTime()
 
-        assertEquals(4, testedObj.remainingTime)
+        assertEquals(0, testedObj.remainingTime)
     }
 
     @Test
+    @DisplayName("when time added, should increment remaining time")
     fun addTime() {
-        testedObj.remainingTime = 0
+        testedObj.initialTime = 1
 
         testedObj.addTime(1)
 
-        assertEquals(1, testedObj.remainingTime)
+        assertEquals(2, testedObj.remainingTime)
     }
 
     @Test
+    @DisplayName("when reset, remaining time should be equal to initial time")
     fun reset() {
-        testedObj.initialTime = 5
+        testedObj.initialTime = 1
         testedObj.advanceTime()
 
         testedObj.reset()
 
-        assertEquals(5, testedObj.remainingTime)
+        assertEquals(1, testedObj.remainingTime)
     }
 }
