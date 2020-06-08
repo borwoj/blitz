aa29fcc32e86b85df0b0e982d9d1ef6d4707bd52
Borys Wojciechowski <borwoj@gmail.com>
2020-06-08 17:29:40 +0200

    update

--- app/src/test/java/net/borysw/blitz/game/GameInfoCreatorImplTest.kt
+++ app/src/test/java/net/borysw/blitz/game/GameInfoCreatorImplTest.kt
@@ -1,13 +1,18 @@
 package net.borysw.blitz.game
 
-import net.borysw.blitz.game.engine.clock.ChessClock
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import net.borysw.blitz.game.engine.clock.ChessClock.Player.Player1
+import net.borysw.blitz.game.engine.clock.ChessClock.Player.Player2
+import net.borysw.blitz.game.engine.clock.ClockStatus
+import net.borysw.blitz.game.status.GameInfo
 import net.borysw.blitz.game.status.GameInfo.Status
 import net.borysw.blitz.game.status.GameInfo.Status.Finished
 import net.borysw.blitz.game.status.GameInfo.Status.InProgress
 import net.borysw.blitz.game.status.GameInfo.Status.Paused
 import net.borysw.blitz.game.status.GameInfo.Status.Unstarted
 import net.borysw.blitz.game.status.GameInfoCreatorImpl
-import net.borysw.blitz.game.status.SecondsTimeFormatterImpl
+import net.borysw.blitz.game.status.TimeFormatter
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
@@ -18,30 +23,30 @@ import org.junit.jupiter.params.provider.MethodSource
 internal class GameInfoCreatorImplTest {
     companion object {
         @JvmStatic
-        fun getArguments(): List<Arguments> {
+        fun parameters(): List<Arguments> {
             return listOf(
-                of(10, 10, 10, null, Unstarted),
-                of(10, 9, 10, null, Paused),
-                of(10, 0, 5, ChessClock.Player.Player1, Finished.Player1Won),
-                of(10, 5, 0, ChessClock.Player.Player2, Finished.Player2Won),
-                of(10, 1, 5, ChessClock.Player.Player1, InProgress.Player1Turn),
-                of(10, 2, 5, ChessClock.Player.Player2, InProgress.Player2Turn)
+                of(ClockStatus(10, 10, 10, 10, 10, null), Unstarted),
+                of(ClockStatus(10, 9, 10, 10, 10, null), Paused),
+                of(ClockStatus(10, 0, 5, 10, 10, Player1), Finished.Player1Won),
+                of(ClockStatus(10, 5, 0, 10, 10, Player2), Finished.Player2Won),
+                of(ClockStatus(10, 1, 5, 10, 10, Player1), InProgress.Player1Turn),
+                of(ClockStatus(10, 2, 5, 10, 10, Player2), InProgress.Player2Turn)
             )
         }
     }
 
-    @ParameterizedTest(name = "when initialTime: {0}, timeLeftA: {1}, timeLeftB: {2}, current: {3}, then return {4}")
-    @MethodSource("getArguments")
+    @ParameterizedTest(name = "when clock status: {0}, then game info should be: {1}")
+    @MethodSource("parameters")
     @DisplayName("create game status")
-    fun getStatus(
-        initialTime: Long,
-        timeLeftA: Long,
-        timeLeftB: Long,
-        current: ChessClock.Player?,
+    fun getInfo(
+        clockStatus: ClockStatus,
         expectedGameStatus: Status
     ) {
-        val testedObj = GameInfoCreatorImpl(SecondsTimeFormatterImpl())
-        val gameStatus = testedObj.get(initialTime, timeLeftA, timeLeftB, current).status
-        assertEquals(expectedGameStatus, gameStatus)
+        val timeFormatter = mock<TimeFormatter> {
+            on(it.format(any())).thenReturn("0")
+        }
+        val testedObj = GameInfoCreatorImpl(timeFormatter)
+        val gameStatus = testedObj.get(clockStatus)
+        assertEquals(GameInfo("0", "0", "0", "0", expectedGameStatus), gameStatus)
     }
 }
